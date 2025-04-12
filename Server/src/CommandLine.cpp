5ffad06b97c8475853747390cf3f313323539e3a
chapeullah <chapeullah@gmail.com>
2025-04-12 19:03:19 +0300

    Update files

--- Server/src/CommandLine.cpp
+++ Server/src/CommandLine.cpp
@@ -1,23 +1,21 @@
 #include <thread>
 #include <iostream>
 #include <array>
+#include <chrono>
+#include <windows.h>
 
 #include "CommandLine.h"
-
-constexpr std::array<const char*, 4> serverCommands_=
-{
-    "Start",
-    "Restart",
-    "Stop",
-    "Exit"
-};
+#include "Logger.h"
 
 void CommandLine::consoleThread()
 {
     auto loop = [this]() 
     {
+        std::cout << "\x1b[?25l";
+        for (const char* string : kServerCommands_) std::cout << "  " << string << "\n";
         while (true)
         {
+            consoleInput();
             consoleDraw();
         }
     };
@@ -25,11 +23,40 @@ void CommandLine::consoleThread()
     cinThread_.detach();
 }
 
-void CommandLine::consoleDraw() // ДОДЕЛАТЬ - ОШИБКА
+void CommandLine::consoleInput() 
 {
-    for (const char *i : serverCommands_)
+    if ((GetAsyncKeyState(VK_DOWN) & 0x8000) && ((static_cast<int>(currentCommand_) + 1) < 4)&& !isPressed_)
     {
-        std::cout << "- " << i << "\n";
+        isPressed_ = true;
+        moveCursor(0, static_cast<int>(currentCommand_));
+        std::cout << " ";
+        currentCommand_ = static_cast<Command>(static_cast<int>(currentCommand_) + 1);
+        Logger::info("CMD", 
+            std::string("Pressed DOWN - CommandLine::currentCommand_ = ") 
+            + kServerCommands_[static_cast<size_t>(currentCommand_)]);
+    } 
+    else if ((GetAsyncKeyState(VK_UP) & 0x8000) && ((static_cast<int>(currentCommand_) - 1) >= 0) && !isPressed_)
+    {
+        isPressed_ = true;
+        moveCursor(0, static_cast<int>(currentCommand_));
+        std::cout << " ";
+        currentCommand_ = static_cast<Command>(static_cast<int>(currentCommand_) - 1);
+        Logger::info("CMD", 
+            std::string("Pressed UP - CommandLine::currentCommand_ = ") 
+            + kServerCommands_[static_cast<size_t>(currentCommand_)]);
     }
-    std::cout << "\x1b[2K";
+    else if (!(GetAsyncKeyState(VK_DOWN) & 0x8000) && !(GetAsyncKeyState(VK_UP) & 0x8000)) 
+        isPressed_ = false;
+}
+
+void CommandLine::consoleDraw()
+{
+    moveCursor(0, static_cast<int>(currentCommand_));
+    std::cout << ">";
+}
+
+void CommandLine::moveCursor(short x, short y)
+{
+    COORD cursorPos = {x, y};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cursorPos);
 }
--- Server/src/CommandLine.h
+++ Server/src/CommandLine.h
@@ -4,13 +4,28 @@
 #include <array>
 #include <string>
 
+enum class Command
+{
+    Start,
+    Restart,
+    Stop,
+    Exit
+};
+
 class CommandLine
 {
 public:
     void consoleThread();
+    void consoleInput();
     void consoleDraw();
+    void moveCursor(short x, short y);
 
 private:
     std::thread cinThread_;
-    std::array<const char*, 4> serverCommands_;
+    inline static constexpr std::array<const char*, 4> kServerCommands_ = 
+    {
+        "Start", "Restart", "Stop", "Exit"
+    };
+    Command currentCommand_ = Command::Start;
+    bool isPressed_ = false;
 };
